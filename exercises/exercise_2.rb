e1b75eb9cfeb5485a749979fbaa2cf4a9f958771
Anastasia Kuzmin <anakuzmin2@gmail.com>
2023-04-11 00:20:00 +0000

    first commit to exercise 6

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -1,7 +1,13 @@
-require_relative '../setup'
+   require_relative '../setup'
 require_relative './exercise_1'
 
 puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
+@store1.name = "Billy"
+@store1.save
+puts @store1.name
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 85)
+@store1.employees.create(first_name: "Corvin", last_name: "Pokemons", hourly_rate: 100)
+@store2.employees.create(first_name: "Leam", last_name: "Rain", hourly_rate: 64)
+@store2.employees.create(first_name: "Goron", last_name: "James", hourly_rate: 99)
+@store1.employees.create(first_name: "Ronin", last_name: "Chang", hourly_rate: 75)
+@store1.employees.create(first_name: "Fahria", last_name: "Prling", hourly_rate: 55)
